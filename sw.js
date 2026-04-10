ad94fcc6238ca7e1d4152167868f179b83697f08
sommerstexan-lgtm <sommers.texan@gmail.com>
2026-04-10 09:33:33 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = 'clinbridge-v9.10.43';
-const ASSETS = ['./index.html','./manifest.json'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+var CACHE_NAME = 'clinbridge-v9.10.48';
+var ASSETS = ['./', './index.html'];
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE_NAME).then(function(c) { return c.addAll(ASSETS); }));
   self.skipWaiting();
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-  ));
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
+  }));
   self.clients.claim();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
 });
