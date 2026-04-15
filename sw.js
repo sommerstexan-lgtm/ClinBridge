645af922001bef2ee32e6f49ca3f878152fb99c7
sommerstexan-lgtm <sommers.texan@gmail.com>
2026-04-15 18:19:58 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ClinBridge Service Worker
 // CACHE_NAME must be updated every release — see VERSION UPDATE CHECKLIST item 8
-const CACHE_NAME = 'clinbridge-v9.10.56';
+const CACHE_NAME = 'clinbridge-v9.10.63';
 
 const ASSETS = [
   './',
