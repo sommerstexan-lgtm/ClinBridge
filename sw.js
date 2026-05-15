f6859df7d32f704f9f4faa3c7ffafa819cf4f088
sommerstexan-lgtm <sommers.texan@gmail.com>
2026-05-15 09:40:16 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-// ClinBridge Service Worker — v9.10.126
-const CACHE_NAME = 'clinbridge-v9.10.126';
+// ClinBridge Service Worker — v9.10.127
+const CACHE_NAME = 'clinbridge-v9.10.127';
 const urlsToCache = [
   './',
   './index.html',
-  './ClinBridgev9_10_126.html',
+  './ClinBridgev9_10_127.html',
   './manifest.json',
   './ClinBridge-App-logo.JPG'
 ];
