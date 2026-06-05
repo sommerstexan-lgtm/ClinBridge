58d1fd1f22c5c285e373ce605e9dd1f93586c623
sommerstexan-lgtm <sommers.texan@gmail.com>
2026-06-05 08:07:04 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'clinbridge-v9.10.180';
+const CACHE_NAME = 'clinbridge-v9.10.181';
 const urlsToCache = ['./', './index.html', './manifest.json', './ClinBridge-App-logo.JPG'];
 self.addEventListener('install', event => {
   self.skipWaiting();
