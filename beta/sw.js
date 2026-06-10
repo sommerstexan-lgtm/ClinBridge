3b66607ba96ac4d1d6a66379d54b3c7acf8b7e64
sommerstexan-lgtm <sommers.texan@gmail.com>
2026-06-10 16:43:02 -0500

    Add files via upload

---
+++ beta/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'clinbridge-beta-v9.10.205';
+const urlsToCache = ['./', './index.html', './manifest.json', '../ClinBridge-App-logo.JPG'];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
